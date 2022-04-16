7ca1ba37fcf17454e305b226f99f1c1a9624aac5
aaliyahsambale <103512908+aaliyahsambale@users.noreply.github.com>
2022-04-16 17:18:32 +0800

    yes

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     title: 'Art Murals for Leni-Kiko in Marikina',
-    subtitle: 'Mga Artista at kabataang tumitindig para sa kulay rosas na bukas',
+    subtitle: 'Mga Artista at kabataang tumitindig para sa kulay rosas na bukas.',
     byline: 'By: Aaliyah Sambale & Maning Sambale',
     footer: 'Source: <a href="https://github.com/aaliyahsambale/rosasmurals" target="_blank">Code</a> and <a href="https://github.com/aaliyahsambale/rosasmurals/images/" target="_blank">images</a>. <br> Thank you Youth for Vote for Leni - Marikina City and <a href="https://www.youtube.com/watch?v=-L-MW7Q8hhg&list=PLSQP2MHpAR2MAlIGYwL-mWs5NLRJeLKin" target="_blank">Pinoy Adventure Rider</a> for sharing the location of the murals. <br> Join this Facebook Group: Youth Vote for Leni - Marikina City and Murals for Leni - Volunteers 🌸🌸🌸. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
